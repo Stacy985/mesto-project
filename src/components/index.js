c323d47c079c3b669f79cf6e1b89dd823c167ff5
Stacy985» git config --global user.email «Anastasia.disy@gmail.com»git config --global user.name Stacy985» <stasbasov@yandex.ru>
2022-07-06 21:32:03 +0300

    место Вебпак

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,15 @@
+import avatar from '../images/Avatar.png';
+import logo from '../images/logo.svg';
+import buttonImgEdit from '../images/Edit_Button__1.svg';
+import buttonImgAdd from '../images/Add_Button___.svg';
+const imgLocal = [
+    // меняем исходные пути на переменные
+    { name: 'Avatar', image: avatar },
+    { name: 'Logo Mesto', link: logo },
+    { name: 'Pan', link: buttonImgEdit },
+    { name: 'Plus', link: buttonImgAdd },
+  ]; 
+import '../pages/index.css';
 import {imagePopup, profilePopup, buttonEdit, nameInput,informationInput, buttonCloseProfile, profileForm, nameTitle, profileSubtitle,buttonCloseImg,buttonAdd,cardPopup, picPopup, imgSubtitle, cardPopupCloseButton } from './modal.js'; 
 import {openPopup, closePopup, keyHandlerEsc, createName, closeByClickOverlay, handleClickImage} from './modal.js'; 
 import {validationConfig, showError, hideError, checkInpitValidity, toggleButtonState, setEventListener, enableValidation, resetCardForm, resetProfifleForm } from './validate.js'; 
